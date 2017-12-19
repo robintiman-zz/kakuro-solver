5a392b690220117e716055872e587cb3be5cf995
Robin Timan <robintiman@gmail.com>
2017-12-19 12:39:13 +0000

    Recursive method for finding combinations done.

--- kakuro.py
+++ kakuro.py
@@ -2,24 +2,25 @@ import numpy as np
 import sys
 
 """
+Kakuro game solver.
+Demo example from p.119 in book. 
+
 n = number of spots
 S = sum to add up to
 """
 
-def find_combos(S, n, available, picked):
-    curr_sum = sum(picked)
-    if curr_sum == S and len(picked) == n:
+def find_combos(S, n, available, picked=[], partial_sum=0):
+    if partial_sum == S and len(picked) == n:
         yield picked
-    if curr_sum > S or len(picked) > n:
+    if partial_sum > S or len(picked) > n:
         return
 
     for i, num in enumerate(available):
-        available = available[i + 1:]
-        picked = picked + [num]
-        yield from find_combos(S, n, available, picked)
+        remaining = available[i + 1:]
+        yield from find_combos(S, n, remaining, picked + [num], partial_sum + num)
 
 n = int(sys.argv[1])
 S = int(sys.argv[2])
 available = [i for i in range(1, 10)]
-for comb in find_combos(S, n, available, []):
-    print(comb)
+for comb in find_combos(S, n, available):
+    print(list(comb), sum(comb))
